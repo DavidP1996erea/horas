b50b7f008e31731b384028c1bf40d133b1d1a71d
David Perea <david.perea@iesnervion.es>
2022-02-01 19:58:08 +0100

    dds

--- src/principal/Hora.java
+++ src/principal/Hora.java
@@ -14,60 +14,68 @@ public class Hora {
 
 
 
+    public void aumentoHoras(){
+        if (horas == 24) {
+            horas = 0;
+        }
 
-    public void aumentosegundos(){
+    }
+
+    public void aumentoMinutos(){
+
+        if (minutos == 60) {
+            minutos = 0;
+            horas++;
+        }
+    }
+
+    public void aumentoSegundos(){
 
         segundos++;
         if(segundos==60) {
             segundos = 0;
             minutos++;
-
-            if (minutos == 60) {
-                minutos = 0;
-                horas++;
-
-                if (horas == 24) {
-                    horas = 0;
-                }
-            }
         }
-
     }
+
+
 
 
 
     public int getSegundos() {
+
         return segundos;
     }
 
-    public void setSegundos(int segundos) {
+    public void setSegundos(int segundos)  throws Exception {
         if (segundos >= 0 && segundos < 60){
             this.segundos = segundos;
       }else if(segundos==60){
             this.segundos=0;
         }else {
-            System.out.println("Número inválido");
+            throw new Exception("Número inválido");
         }
     }
 
 
     public int getMinutos() {
+
         return minutos;
     }
 
-    public void setMinutos(int minutos) throws Exception {
+    public void setMinutos(int minutos) {
 
         if (minutos >= 0 && minutos < 60){
             this.minutos = minutos;
         }else if(minutos==60){
             this.minutos=0;
         }else {
-            throw new Exception("Número inválido");
-            //System.out.println("Número inválido");
+
         }
     }
 
     public int getHoras() {
+
         return horas;
     }
 
--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,36 +1,67 @@
 package principal;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
         Scanner sc = new Scanner(System.in);
 
     int segundos=0;
+    int segundosExtras=0;
+    int horas=0;
+    int minutos=0;
 
 
         Hora hora1 = new Hora();
-        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
-        Date date1= new Date();
+        LocalDate fecha = LocalDate.now();
 
-        LocalTime hora = LocalTime.now();
+        System.out.println("Introduce las horas");
+        horas=sc.nextInt();
 
+        System.out.println("Introduce los minutos");
+        minutos=sc.nextInt();
 
-
-
-        hora1.setSegundos(hora.getSecond());
-        hora1.setHoras(hora.getHour());
-      // hora1.setMinutos(hora.getMinute());
-
-        System.out.println("Hoy es: " + formatoFecha.format(date1) + " y " + hora1.toString());
-
-        System.out.println("Introduzca los segundos");
+        System.out.println("Introduce los segundos");
         segundos=sc.nextInt();
+
+
+
+    try{
+
+        hora1.setSegundos(segundos);
+
+    }catch (Exception e){
+
+        System.out.println("Introduzca un valor válido, entre 0 y 60");
+        segundos=sc.nextInt();
+
+        hora1.setSegundos(segundos);
+    }
+
+
+        hora1.setHoras(horas);
+
+        hora1.setMinutos(minutos);
+
+
+
+        System.out.println("Hoy es: " + fecha.getDayOfMonth() +"/" + fecha.getMonthValue() + "/" + fecha.getYear()  + " y " + hora1.toString());
+
+
+        System.out.println("Introduce los segundos que quieres añadir");
+        segundosExtras=sc.nextInt();
+
+
+        for (int i=0; i<segundosExtras ; i++){
+
+            hora1.aumentoSegundos();
+            System.out.println(hora1.toString());
+
+        }
 
 
 
